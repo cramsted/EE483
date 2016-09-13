a2ffb821f264245b8d67b0d3368e25cd3071c00a
Clayton Ramstedt <clayton.ramstedt@gmail.com>
2016-09-12 20:02:01 -0600

    Init commit. Finished lab1

---
+++ lab1/Whirlybird/Lab01/param.py
@@ -0,0 +1,29 @@
+# Whirlybird Parameter File
+import numpy as np
+
+# From parameters list of the Whirlybird lab documentation
+g = 9.81       # Gravity, m/s**2
+L1 = 0.81      # Length of rod from vertex to the rotors, m
+L2 = 0.3048    # Length of rod from vertex to counterbalance, m
+m1 = 0.891     # Mass of weight at the end of l1 (between the rotors), kg
+m2 = 1         # Mass of counterbalance at the end of l2, kg
+d = 0.178      # Distance between m1 and a rotor (half the distance between rotors), m
+h = 0.65       # Height of support rod, m
+r = 0.12       # Length of one of the square rotors (maybe radius instead?), m
+Jx = 0.0047    # Some force in x direction???, kg-m^2
+Jy = 0.0014    # Some force in y direction???, kg-m^2
+Jz = 0.0041    # Some force in z direction???, kg-m^2
+#km = ????     # On parameters list, will add in later
+#omega_gyro    # "
+#omega_pixel   # "
+
+# Other parameters for the animation
+ell =  5      # For the axis, m
+
+# Simulation Parameters
+Ts = 0.01 
+
+# Initial Conditions
+phi0 = 0.0*np.pi/180    # Roll of Whirlybird relative to the ground, rads
+theta0 = 0.0*np.pi/180  # Pitch of Whirlybird relative to the ground, rads
+psi0 = 0.0*np.pi/180    # Yaw of Whirlybird relative to the ground, rads
